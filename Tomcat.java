e85e7ef850b2c52348995ee1ffb097d7336e7f8c
Peter <schmidt.multimedia@gmail.com>
2021-06-04 09:32:32 +0200

    SuperCat ++ / protected int age / ..extends SuperCat

--- Tomcat.java
+++ Tomcat.java
@@ -1,25 +1,8 @@
-public class Tomcat 
+public class Tomcat extends SuperCat
 {
-    // Property | Attribut | Field
-    private String name;
-    private String furColor;
-    private int age;
 
     public Tomcat(String name, String furColor, int age) {
-        this.name = name;
-        this.furColor = furColor;
-        this.age = age;
-    }
-
-    public String tellYourAttributes(String checkStr) {
-        switch (checkStr) {
-            case "#name":
-                return this.name;
-            case "#color":
-                return this.furColor;
-            default:
-                return "ERROR";
-        }
+        super(name, furColor, age);
     }
 
     public String tellYourAge() {
